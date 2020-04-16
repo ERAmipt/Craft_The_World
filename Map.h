3809aac115c27073c644c6e99c158caf16a59386
alpocnito <ron.zaripovr@gmail.com>
2020-04-16 23:35:35 +0300

    Map

--- Map.h
+++ Map.h
@@ -1,6 +1,101 @@
 #pragma once
-/*
+#include <SFML/Graphics.hpp>
+#include <iostream>
 namespace M {
+
+	const int level[] =
+	{
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	};
+
+	class Map : public sf::Drawable, public sf::Transformable
+	{
+	public:
+
+		bool load()
+		{	
+			unsigned int width = 16;
+			unsigned int height = 8;
+			sf::Vector2u tileSize = { 64, 64 };
+
+			// load the tileset texture
+			if (!m_tileset.loadFromFile("images/blocks.png"))
+				return false;
+
+			// resize the vertex array to fit the level size
+			m_vertices.setPrimitiveType(sf::Quads);
+			m_vertices.resize(width * height * 4);
+
+			// populate the vertex array, with one quad per tile
+			for (unsigned int i = 0; i < width; ++i)
+				for (unsigned int j = 0; j < height; ++j)
+				{
+					// get the current tile number
+					int tileNumber = level[i + j * width];
+					// get a pointer to the current tile's quad
+					sf::Vertex* quad = &m_vertices[(i + j * width) * 4];
+
+					int tu = 0, tv = 0;
+					switch (tileNumber) {
+						case 0:
+							tu = 192;
+							tv = 416;
+							break;
+						case 1:
+							tu = 896;
+							tv = 32;
+							break;
+						default:
+							std::cerr << "Unknown tileNumber";
+							return false;
+					}
+
+					// define its 4 corners
+					quad[0].position = sf::Vector2f(i * tileSize.x, j * tileSize.y);
+					quad[1].position = sf::Vector2f((i + 1) * tileSize.x, j * tileSize.y);
+					quad[2].position = sf::Vector2f((i + 1) * tileSize.x, (j + 1) * tileSize.y);
+					quad[3].position = sf::Vector2f(i * tileSize.x, (j + 1) * tileSize.y);
+
+					// define its 4 texture coordinates
+					quad[0].texCoords = sf::Vector2f(tu, tv);
+					quad[1].texCoords = sf::Vector2f(tu + 64, tv);
+					quad[2].texCoords = sf::Vector2f(tu + 64, tv + 64);
+					quad[3].texCoords = sf::Vector2f(tu, tv + 64);
+				}
+
+			return true;
+		}
+
+	private:
+
+		virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const
+		{
+			// apply the transform
+			states.transform *= getTransform();
+
+			// apply the tileset texture
+			states.texture = &m_tileset;
+
+			// draw the vertex array
+			target.draw(m_vertices, states);
+		}
+
+		sf::VertexArray m_vertices;
+		sf::Texture m_tileset;
+	};
+
+	// create the tilemap from the level definition
+	//Map map;
+	
+}
+	/*
 	class Block
 	{
 	public:
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 #include "Game.h"
+
 bool AnalyseEvent(sf::Event event);
 bool CheckKeyboard();
 bool CheckMouse();
@@ -8,7 +9,11 @@ bool CheckMouse();
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(1920, 1080), "CRAFT THE WORLD", sf::Style::Fullscreen);
-    sf::CircleShape shape(100.f);
+    M::Map map;
+    if (!map.load()) {
+        std::cerr << "Can not load map. Check images folder";
+        return -1;
+    }
 
     while (window.isOpen())
     {   
@@ -23,7 +28,7 @@ int main()
 
 
         window.clear();
-        window.draw(shape);
+        window.draw(map);
         window.display();
     }
 
